de56d03d9b78222341fb8542ac7070391d7d75cc
CodeJorka <jori0x14@gmail.com>
2019-05-08 21:17:06 +0200

    Pradzia

---
+++ java.js
@@ -0,0 +1,22 @@
+let a = 7;
+
+switch(a) {
+	case 3:
+		alert('too small');
+		break;
+	case 4:
+		alert('getting close');
+		break;
+	case 5:
+	case 6:
+		alert('just right');
+		break;
+	case 7:
+		alert('Bingo!');
+		break;
+	default:
+		alert('I don\'t know such numbers');
+}
+
+
+
---
+++ script.js
@@ -0,0 +1,14 @@
+let kaina = prompt("Kokia yra kaina")
+let PVM = 0.21;
+let kainaSuPVM = Number(kaina) + (Number(kaina)*PVM)
+let kainaBePVM = Number(kaina) + (Number(kaina)*PVM)
+let PVMSkaic = Number(kaina)*PVM;
+console.log("Kaina su pvm", kainasuPVM.toFixed(2), )
+console.log("PVM:", PVMskaic.toFixed(2), )
+console.log("Kaina be PVM", kainabePVM.toFixed(2), )
+
+
+
+// let a = 121.382
+// let b = a.toFixed(2);
+// document.write(b);
